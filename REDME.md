5a9584f2ccc42edbbcbdbbc6125e6f8c1376dbcd
Johan Söderlund <js223zs@student.lnu.se>
2019-01-23 10:48:47 +0100

    packages, POST route, readme

---
+++ REDME.md
@@ -0,0 +1,20 @@
+## The web as an application platform - 1DV527
+
+### By Johan Söderlund
+
+# SmartHome
+Aims to solve ...
+
+## Questions
+
+1. How have you implemented the idea of HATEOAS in your API? Motivate your choices and how it support the idea of HATEOAS.
+
+2. If your solution should implement multiple representations of the resources. How would you do it?
+
+3. Motivate and defend your authentication solution? Why did you choose the one you did? Pros/Cons.
+
+4. Explain how your web hook works.
+
+5. Since this is your first own web API there are probably things you would solve in an other way looking back at this assignment. Write your thoughts down.
+
+6. Did you do something extra besides the fundamental requirements? Explain them.
--- index.js
+++ index.js
@@ -1,8 +1,42 @@
-const Koa = require('koa');
-const app = new Koa();
+const Koa = require("koa");
+const Router = require("koa-router");
+const BodyParser = require("koa-bodyparser");
+const logger = require('koa-logger');
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
+
+const app = new Koa();
+const router = new Router();
+
+app.use(BodyParser());
+app.use(logger());
+
+router.get("/", async function (ctx) {
+    let query = JSON.stringify(ctx.request.query);
+    ctx.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, HEAD');
+    ctx.set({accept: 'application/json'});
+    ctx.body = {
+      lightbulbs: [
+        {kitchenLightbulb: {on: true, runtime: 65000}},
+        {bedroomLightbulb: {on: false, runtime: 33000}},
+    ]
+    };
+    ctx.headers = {accept: 'application/json'}
 });
+
+router.post("/", async function (ctx) {
+    //if typeof(ctx.request.body) === object
+    let body = JSON.stringify(ctx.request.body);
+    let query = JSON.stringify(ctx.request.query);
+    ctx.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, HEAD');
+    ctx.set({accept: 'application/json'});
+    ctx.body = {
+      lightbulbs: [
+        {kitchenLightbulb: {on: true, runtime: 65000}},
+        {bedroomLightbulb: {on: false, runtime: 33000}},
+    ]
+    };
+});
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000);
